558f46d4e174262bfeeea37767035f25df8935e9
Hilton Shumway <hillshum@gmail.com>
2018-11-19 23:30:13 -0600

    Code cleanup

--- src/util/get-id.js
+++ src/util/get-id.js
@@ -3,7 +3,7 @@ const POSSIBLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 export default function makeid() {
   let text = "";
 
-  for (var i = 0; i < 6; i++)
+  for (var i = 0; i < LEN; i++)
     text += POSSIBLE.charAt(Math.floor(Math.random() * POSSIBLE.length));
 
   return text;
